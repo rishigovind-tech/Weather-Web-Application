345e18c4b2172f495c1454d97f6823e9143f57d0
RISHIGOVIND <rishigovind36@gmail.com>
2025-02-13 09:20:26 +0530

    api changed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "" //api key
+const apiKey = "your api key" 
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
 const searchBox = document.querySelector(".search input")
